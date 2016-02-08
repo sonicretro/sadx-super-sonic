09a39fd80647b48c3f73f976a901cc54b171469d
SonicFreak94 <sonicfreak94@hotmail.com>
2016-02-08 16:07:20 -0700

    Added ring loss and music change

--- sadx-super-sonic/mod.cpp
+++ sadx-super-sonic/mod.cpp
@@ -5,8 +5,12 @@
 
 DataPointer(NJS_TEXLIST, SUPERSONIC_TEXLIST, 0x0142272C);
 DataPointer(bool, SuperSonicFlag, 0x03C55D45);
+DataPointer(int, CurrentSong, 0x00912698);
+DataPointer(int, LastSong, 0x0091269C);
+DataPointer(Bool, Music_Enabled, 0x0091268C);
 
-static const PVMEntry SuperSonicPVM = { "SUPERSONIC", &SUPERSONIC_TEXLIST };
+static int ring_timer = 0;
+static int super_count = 0;	// Dirty hack for multitap mod compatibility
 
 void __cdecl Sonic_SuperPhysics_Delete(ObjectMaster* _this)
 {
@@ -16,6 +20,40 @@ void __cdecl Sonic_SuperPhysics_Delete(ObjectMaster* _this)
 	if (data2 != nullptr)
 		data2->PhysicsData = PhysicsArray[Characters_Sonic];
 }
+
+// TODO: Figure out why this runs on the result screen
+void __cdecl SuperSonicManager_Main(ObjectMaster* _this)
+{
+	if (super_count < 1)
+	{
+		DeleteObject_(_this);
+		return;
+	}
+
+	if (Music_Enabled && CurrentSong != 86)
+		CurrentSong = 86;
+
+	++ring_timer %= 60;
+
+	if (!ring_timer)
+		AddRings(-1);
+}
+void __cdecl SuperSonicManager_Delete(ObjectMaster* _this)
+{
+	super_count = 0;
+	ring_timer = 0;
+
+	if (Music_Enabled)
+		CurrentSong = LastSong;
+}
+void SuperSonicManager_Load()
+{
+	ObjectMaster* obj = LoadObject(0, 2, SuperSonicManager_Main);
+	if (obj)
+		obj->DeleteSub = SuperSonicManager_Delete;
+}
+
+static const PVMEntry SuperSonicPVM = { "SUPERSONIC", &SUPERSONIC_TEXLIST };
 
 extern "C"
 {
@@ -30,6 +68,7 @@ extern "C"
 		if (GameState != 15)
 			return;
 
+		// TODO: Jump + Cancel to transform, 50 ring requirement
 		for (Uint32 i = 0; i < 8; i++)
 		{
 			CharObj1* data1 = CharObj1Ptrs[i];
@@ -38,28 +77,36 @@ extern "C"
 			if (data1 == nullptr || data1->CharID != Characters_Sonic)
 				continue;
 
-			if (ControllerPointers[i]->PressedButtons & Buttons_Z)
+			bool toggle = (ControllerPointers[i]->PressedButtons & Buttons_Z) != 0;
+			// I'm confused by this. Every time I check this, that bit isn't there,
+			// but the super physics object checks for that bit and it IS there,
+			// otherwise it would restore the original physics.
+			// Consequently, I'm setting the bit myself.
+			if ((data2->Upgrades & Upgrades_SuperSonic) != Upgrades_SuperSonic)
 			{
-				// I'm confused by this. Every time I check this, that bit isn't there,
-				// but the super physics object checks for that bit and it IS there,
-				// otherwise it would restore the original physics.
-				// Consequently, I'm setting the bit myself.
-				if ((data2->Upgrades & Upgrades_SuperSonic) != Upgrades_SuperSonic)
+				if (toggle)
 				{
 					// Transform into Super Sonic
 					data1->NextAction = 46;
 					data1->Status |= Status_DoNextAction;
 					data2->Upgrades |= Upgrades_SuperSonic;
-				}
-				else
-				{
-					// Change back to normal Sonic
-					data1->NextAction = 47;
-					data1->Status |= Status_DoNextAction;
-					data2->Upgrades &= ~Upgrades_SuperSonic;
+					PlayVoice(396);
+
+					if (!super_count++)
+						SuperSonicManager_Load();
 				}
 			}
+			else if (toggle || !Rings)
+			{
+				// Change back to normal Sonic
+				data1->NextAction = 47;
+				data1->Status |= Status_DoNextAction;
+				data2->Upgrades &= ~Upgrades_SuperSonic;
+				--super_count;
+			}
 		}
+
+		SuperSonicFlag = super_count > 0;
 	}
 
 	EXPORT ModInfo SADXModInfo = { ModLoaderVer };
